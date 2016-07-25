c8f97b8c46717a7593e3c86a76d7ab5f3d057dd0
Sawyer Charles <xssc@users.noreply.github.com>
2016-07-25 13:12:18 -0500

    Switched to httparty to support ruby 1.9.3 and not require libcurl

--- lib/gpsoauth.rb
+++ lib/gpsoauth.rb
@@ -3,23 +3,21 @@ require "gpsoauth/google"
 require "gpsoauth/utilities"
 require "base64"
 require "openssl"
-require "typhoeus"
+require "httparty"
 
 module Gpsoauth
 	class Auth
 		class << self
-			B64_KEY = ("AAAAgMom/1a/v0lblO2Ubrt60J2gcuXSljGFQXgcyZWveWLEwo6prwgi3iJIZdodyhKZQrNWp5nKJ3srRXcUW+F1BD3baEVGcmEgqaLZUNBjm057pKRI16kB0YppeGx5qIQ5QjKzsR8ETQbKLNWgRY0QRNVz34kMJR3P/LgHax/6rmf5AAAAAwEAAQ==")
+			B64_KEY     = ("AAAAgMom/1a/v0lblO2Ubrt60J2gcuXSljGFQXgcyZWveWLEwo6prwgi3iJIZdodyhKZQrNWp5nKJ3srRXcUW+F1BD3baEVGcmEgqaLZUNBjm057pKRI16kB0YppeGx5qIQ5QjKzsR8ETQbKLNWgRY0QRNVz34kMJR3P/LgHax/6rmf5AAAAAwEAAQ==")
 			ANDROID_KEY = Google.keyFromb64(B64_KEY)
+			AUTH_URL    = 'https://android.clients.google.com/auth'
+			USER_AGENT  = "gpsoauth/1.0.0"
 
-			AUTH_URL = 'https://android.clients.google.com/auth'
+			@@cookie    = ""
 
-			USER_AGENT = "gpsoauth/1.0.0"
-			COOKIES    = './cookies-google'
-
-			def performAuthRequest(data, cookiefile = COOKIES, cookiejar = COOKIES)
-				request = Typhoeus::Request.new(AUTH_URL, method: :post, body: data, headers: {'User-Agent': USER_AGENT}, cookiefile: cookiefile, cookiejar: cookiejar)
-			    request.run
-				response = request.response
+			def performAuthRequest(data, cookie = @@cookie)
+				response = HTTParty.post(AUTH_URL, body: data, headers: {'User-Agent': USER_AGENT, 'Cookie': cookie})
+				@@cookies = response.headers['Set-Cookie'] if response.headers['Set-Cookie']
 				return Google.parseAuthResponse(response.body)
 			end
 
@@ -38,7 +36,7 @@ module Gpsoauth
 			        'lang':    lang,
 			        'sdk_version': sdk_version
 			    }
-			    return performAuthRequest(data, nil, COOKIES)
+			    return performAuthRequest(data, "")
 			end
 
 			def performOAuth(email, master_token, android_id, service, app, client_sig, device_country = 'us', operatorCountry = 'us', lang = 'en', sdk_version = 17)
